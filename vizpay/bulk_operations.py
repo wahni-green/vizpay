d41f90a70d358c3a37b480be0f67b48ee3e804d2
stevewilson9009 <stevew9009@gmail.com>
2025-04-03 05:47:56 +0200

    fix: correct 'year' in license

--- vizpay/bulk_operations.py
+++ vizpay/bulk_operations.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, Wahni IT Solutions Pvt Ltd and contributors
+# Copyright (c) 2025, Wahni IT Solutions Pvt Ltd and contributors
 # For license information, please see license.txt
 
 import frappe
