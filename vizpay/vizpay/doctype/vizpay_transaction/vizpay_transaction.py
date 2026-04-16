aa0c951d13f0cc936f9630ca41184cdd129e62d7
visrm <yveltal4286@gmail.com>
2026-04-16 11:21:57 +0530

    fix: coderabbit suggested fix 1

--- vizpay/vizpay/doctype/vizpay_transaction/vizpay_transaction.py
+++ vizpay/vizpay/doctype/vizpay_transaction/vizpay_transaction.py
@@ -96,7 +96,10 @@ class VizpayTransaction(Document):
 		payment_entry.setup_party_account_field()
 		payment_entry.set_missing_values()
 
-		payment_entry.sales_person = frappe.db.get_value("Sales Person", {"user": payment_entry.owner}, "name")
+		sales_user = self.owner
+		sales_person = frappe.db.get_value("Sales Person", {"user": sales_user}, "name")
+		if sales_person:
+			payment_entry.sales_person = sales_person
 
 		if auto_allocate:
 			outstanding_docs = get_outstanding_reference_documents(
